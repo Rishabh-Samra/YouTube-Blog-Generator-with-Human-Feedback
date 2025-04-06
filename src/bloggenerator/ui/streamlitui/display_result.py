bc5c5efd90dc09ba0eebef66bddae6179ce90a22
Rishabh-Samra <samrarishabh@gmail.com>
2025-04-06 12:55:18 +0530

    debugging...

--- src/bloggenerator/ui/streamlitui/display_result.py
+++ src/bloggenerator/ui/streamlitui/display_result.py
@@ -11,13 +11,15 @@ class DisplayResultStreamlit:
     def display_result_on_ui(self):
         graph = self.graph
         user_message = self.user_message
+        print("user_message: ", user_message)
         for event in graph.stream({'messages':("user",user_message)}):
             print("event values: ", event.values())
+            if not event or not hasattr(event, "values"):
+                print(f"Error: {event} is empty or lacks 'values'")
+                continue  # Skip this iteration
             for value in event.values():
                 print("value messages: ", value['messages'])
                 with st.chat_message("user"):
                     st.write(user_message)
                 with st.chat_message("assistant"):
                     st.write(value["messages"].content)
-
-             
